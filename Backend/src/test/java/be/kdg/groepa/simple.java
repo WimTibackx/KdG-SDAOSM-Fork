179815ed9a4982495428057f2daaf855456fb0ce
Thierry <thierry.van.loo.tvl@gmail.com>
2014-02-05 09:35:45 +0100

    changes in tests
    Signed-off-by: Thierry <thierry.van.loo.tvl@gmail.com>

---
+++ Backend/src/test/java/be/kdg/groepa/simple.java
@@ -0,0 +1,16 @@
+package be.kdg.groepa;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Thierry on 5/02/14.
+ */
+public class simple {
+    @Test
+    public void test(){
+        assertEquals(4,4);
+    }
+}
