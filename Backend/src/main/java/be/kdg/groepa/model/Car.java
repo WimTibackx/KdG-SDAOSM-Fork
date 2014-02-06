e434d03f6c4a3e7b5bfb8ea7049970538b1e43d6
Pieter-Jan Van de Perre <pieter-jan.vandeperre@student.kdg.be>
2014-02-06 15:57:27 +0100

    Register User + First tests (more to come) + Car (WIP)
    Signed-off-by: Pieter-Jan Van de Perre <pieter-jan.vandeperre@student.kdg.be>

---
+++ Backend/src/main/java/be/kdg/groepa/model/Car.java
@@ -0,0 +1,13 @@
+package be.kdg.groepa.model;
+
+/**
+ * Created by Pieter-Jan on 5-2-14.
+ */
+public class Car {
+
+    private String type;
+    private String brand;
+    private double consumption;
+    // private String pictureURL;
+
+}
