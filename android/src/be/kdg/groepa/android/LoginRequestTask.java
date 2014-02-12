73610b141a7a9702787cf904a75bc93bbe07a935
Wim Tibackx <wim.tibackx@student.kdg.be>
2014-02-12 09:19:58 +0100

    Android request to backend FINALLY WORKS. YIPPIE :)
    Signed-off-by: Wim Tibackx <wim.tibackx@student.kdg.be>

--- android/src/be/kdg/groepa/android/LoginRequestTask.java
+++ android/src/be/kdg/groepa/android/LoginRequestTask.java
@@ -1,7 +1,9 @@
 package be.kdg.groepa.android;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 import org.apache.http.HttpResponse;
@@ -10,6 +12,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
@@ -20,6 +23,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -54,19 +58,21 @@ public class LoginRequestTask extends AsyncTask<Void, Void, String> {
         List<NameValuePair> param = new ArrayList<>(1);
         param.add(new BasicNameValuePair("data",jsonObject.toString()));
 
-        HttpGet httpPost = new HttpGet("http://192.168.1.2:8080/BackEnd/login");
-        //try {
-        //httpPost.setEntity(new UrlEncodedFormEntity(param));
-        HttpParams p = new BasicHttpParams();
-        p.setParameter("data",jsonObject.toString());
-        httpPost.setParams(p);
-                /*} catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }*/
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.context);
+        PreferenceManager.setDefaultValues(this.context, R.xml.preferences, false);
+        String serverAddr = preferences.getString("carpoolServer","127.0.0.1:8080");
+
+        String url = "http://"+serverAddr+"/BackEnd/login?";
+        List<NameValuePair> params = new LinkedList<>();
+        params.add(new BasicNameValuePair("data", jsonObject.toString()));
+        url += URLEncodedUtils.format(params, "utf-8");
+
+        HttpGet httpPost = new HttpGet(url);
 
         try {
             response = httpclient.execute(httpPost);
             StatusLine statusLine = response.getStatusLine();
+            Log.d("Login statusline","Statusline is "+statusLine.getStatusCode()+" - "+statusLine.getReasonPhrase());
             if(statusLine.getStatusCode() == HttpStatus.SC_OK){
                 ByteArrayOutputStream out = new ByteArrayOutputStream();
                 response.getEntity().writeTo(out);
@@ -78,9 +84,12 @@ public class LoginRequestTask extends AsyncTask<Void, Void, String> {
                 throw new IOException(statusLine.getReasonPhrase());
             }
         } catch (IOException e) {
+            //Toast toast = Toast.makeText(this.context, "IOEXCEPTION", 500);
+            //toast.show();
+            Log.e("IOExc at Login",e.getMessage());
             //TODO Handle problems..
         }
-        Log.d("Login","ResponseString = "+responseString);
+        Log.d("Login","ResponseString at "+serverAddr+" = "+responseString);
         return responseString;
     }
 
