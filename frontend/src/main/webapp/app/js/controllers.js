fda77a145ce652b6fa209cdf88fa7a6f410256e2
Thierry <thierry.van.loo.tvl@gmail.com>
2014-02-18 20:09:17 +0100

    Added frontend login/ profile still has to be done
    Signed-off-by: Thierry <thierry.van.loo.tvl@gmail.com>

--- frontend/src/main/webapp/app/js/controllers.js
+++ frontend/src/main/webapp/app/js/controllers.js
@@ -1,43 +1,61 @@
 var carpoolingControllers = angular.module('carpoolingControllers', []);
 var token;
 
+
 carpoolingControllers.controller('loginCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     var rootUrl = "http://localhost:8080/BackEnd/";
+
+    $("#cssmenu").hide();
 
     $(document).ready(function () {
         var login = $("#loginform");
         login.submit(function (e) {
             e.preventDefault();
-            var username = login.children('[name="username"]').val();
-            var password = login.children('[name="password"]').val();
+            var username=$("[name=username]").val();
+            var password=$("[name=password]").val();
             actionLogin(username, password);
         });
     });
-
-    function prepData(data) {
-        return {data: JSON.stringify(data)};
-    }
 
     function actionLogin(username, password) {
         var data = {username: username, password: password};
         $.ajax({
             url: rootUrl + "login/",
             method: "POST",
-            data: prepData(data),
+            contentType: "text/plain; charset=utf-8",
+            data:JSON.stringify(data),
             success: function (response) {
-                //console.log(response);
-                token = response;
-                receivedToken(token);
+                var obj = JSON.parse(response);
+                console.log(obj);
+                if(obj.hasOwnProperty("Token")){
+                    console.log("Test");
+                    window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
+                    $("#cssmenu").show();
+                    $("#error").hide();
+                }else if (obj.hasOwnProperty("error")) {
+
+                    if(obj["error"] == "LoginComboWrong") {
+                        $("#error").text("Combination username/password is wrong");
+                        $("#error").show();
+                        $("#cssmenu").hide();
+
+                    }
+                    if(obj["error"] == ("ParseError")) {
+                        $("#error").text("There is a problem with our server, please try again later");
+                        $("#error").show();
+                        $("#cssmenu").hide();
+                    }
+                }
             }
         })
-    }
-
-    function receivedToken(token){
-        $location.url('/myProfile');
     }
 }]);
 
 carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.avatarsrc = '../app/img/avatar.JPG';
     $scope.gendersrc = '../app/img/female.png';
+
+    var rootUrl = "http://localhost:8080/frontend/app/index.html#/myProfile";
+
+
 }]);
