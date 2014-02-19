c3068543b8592714f30dbd7cacc2bd11f0f8eb99
warri93 <melissa.warrens@gmail.com>
2014-02-19 14:04:29 +0100

    register front end

--- frontend/src/main/webapp/app/js/controllers.js
+++ frontend/src/main/webapp/app/js/controllers.js
@@ -8,87 +8,150 @@ carpoolingControllers.controller('loginCtrl', ['$scope', '$http', '$location', f
 
     console.log("HELLOOOOOO");
     console.log(readCookie("Token"));
-    if (readCookie("Token") == null){
-        $("#cssmenu").hide();
+    if (readCookie("Token") == null) {
 
-        $(document).ready(function () {
-            var login = $("#loginform");
-            login.submit(function (e) {
-                e.preventDefault();
-                var username=$("[name=username]").val();
-                var password=$("[name=password]").val();
-                actionLogin(username, password);
-            });
+        var login = document.getElementById('login');
+        var menu = document.getElementById('cssmenu');
+        menu.style.display = 'none';
+        var register = document.getElementById('register');
+        var password = document.getElementById('password');
+        document.getElementById('loginIcon').addEventListener('click', function () {
+            login.style.display = 'block';
+            register.style.display = 'none';
+            password.style.display = 'none';
+        });
+        document.getElementById('registerIcon').addEventListener('click', function () {
+            register.style.display = 'block';
+            password.style.display = 'none';
+            login.style.display = 'none';
+        });
+        document.getElementById('passwordIcon').addEventListener('click', function () {
+            password.style.display = 'block';
+            register.style.display = 'none';
+            login.style.display = 'none';
         });
 
-        function actionLogin(username, password) {
-            var data = {username: username, password: password};
-            $http({
-                method: 'POST',
-                url: rootUrl + "login/",
-                data: JSON.stringify(data),
-                headers: {'Content-Type': "text/plain; charset=utf-8"}
-            }).success(function (response) {
-                    console.log(response)
-                    //var obj = JSON.parse(response);
-                    obj = response;
-                    console.log(obj);
-                    if(obj.hasOwnProperty("Token")){
-                        console.log("Test");
-                        window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
-                        $("#cssmenu").show();
-                        $("#error").hide();
-                    }else if (obj.hasOwnProperty("error")) {
 
-                        if(obj["error"] == "LoginComboWrong") {
-                            $("#error").text("Combination username/password is wrong");
-                            $("#error").show();
-                            $("#cssmenu").hide();
+        if ($.cookie("Token") == null) {
 
-                        }
-                        if(obj["error"] == ("ParseError")) {
-                            $("#error").text("There is a problem with our server, please try again later");
-                            $("#error").show();
-                            $("#cssmenu").hide();
-                        }
-                    }
+            $("#cssmenu").hide();
+
+            $(document).ready(function () {
+                var login = $("#loginform");
+                var registerform = $("#registerform");
+
+                login.submit(function (e) {
+                    e.preventDefault();
+                    var username = $("[name=username]").val();
+                    var password = $("[name=password]").val();
+
+                    actionLogin(username, password);
                 });
-            /*$.ajax({
+
+                registerform.submit(function (e) {
+                    e.preventDefault();
+                    var usernameR = $("[name=usernameRegister]").val();
+                    var passwordR = $("[name=passwordRegister]").val();
+                    var nameR = $("[name=nameRegister]").val();
+                    var smokerR = $("[name=smokerRegister]").val();
+                    var genderR = $("[name=genderRegister]").val();
+                    var dateofbirthR = $("[name=dateofbirthRegister]").val();
+
+                    actionRegister(usernameR, passwordR, nameR, smokerR, genderR, dateofbirthR);
+                });
+            });
+
+            function actionLogin(username, password) {
+                var data = {username: username, password: password};
+                $http({
+                    method: 'POST',
+                    url: rootUrl + "login/",
+                    data: JSON.stringify(data),
+                    headers: {'Content-Type': "text/plain; charset=utf-8"}
+                }).success(function (response) {
+                        console.log(response)
+                        //var obj = JSON.parse(response);
+                        obj = response;
+                        console.log(obj);
+                        if (obj.hasOwnProperty("Token")) {
+                            console.log("Test");
+                            window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
+                            $("#cssmenu").show();
+                            $("#error").hide();
+                        } else if (obj.hasOwnProperty("error")) {
+
+                            if (obj["error"] == "LoginComboWrong") {
+                                $("#error").text("Combination username/password is wrong");
+                                $("#error").show();
+                                $("#cssmenu").hide();
+
+                            }
+                            if (obj["error"] == ("ParseError")) {
+                                $("#error").text("There is a problem with our server, please try again later");
+                                $("#error").show();
+                                $("#cssmenu").hide();
+                            }
+                        }
+                    });
+                /*$.ajax({
+                 url: rootUrl + "login/",
+                 method: "POST",
+                 contentType: "text/plain; charset=utf-8",
+                 data: JSON.stringify(data),
+                 success: function (response) {
+                 var obj = JSON.parse(response);
+                 console.log(obj);
+                 if (obj.hasOwnProperty("Token")) {
+                 console.log("Test");
+                 window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
+                 $("#cssmenu").show();
+                 $("#error").hide();
+                 } else if (obj.hasOwnProperty("error")) {
+
+                 if (obj["error"] == "LoginComboWrong") {
+                 $("#error").text("Combination username/password is wrong");
+                 $("#error").show();
+                 $("#cssmenu").hide();
+
+                 }
+                 if (obj["error"] == ("ParseError")) {
+                 $("#error").text("There is a problem with our server, please try again later");
+                 $("#error").show();
+                 $("#cssmenu").hide();
+                 }
+                 }
+                 }
+                 })    */
+            }
+
+        } else {
+            window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
+        }
+
+        function actionRegister(usernameR, passwordR, nameR, smokerR, genderR, dateofbirthR) {
+            var data = {username: usernameR, password: passwordR, name: nameR, smoker: smokerR, gender: genderR, dateofbirth: dateofbirthR};
+            $.ajax({
                 url: rootUrl + "login/",
                 method: "POST",
                 contentType: "text/plain; charset=utf-8",
-                data:JSON.stringify(data),
+                data: JSON.stringify(data),
                 success: function (response) {
                     var obj = JSON.parse(response);
                     console.log(obj);
-                    if(obj.hasOwnProperty("Token")){
+                    if (obj.hasOwnProperty("Token")) {
                         console.log("Test");
                         window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
                         $("#cssmenu").show();
                         $("#error").hide();
-                    }else if (obj.hasOwnProperty("error")) {
-
-                        if(obj["error"] == "LoginComboWrong") {
-                            $("#error").text("Combination username/password is wrong");
-                            $("#error").show();
-                            $("#cssmenu").hide();
-
-                        }
-                        if(obj["error"] == ("ParseError")) {
-                            $("#error").text("There is a problem with our server, please try again later");
-                            $("#error").show();
-                            $("#cssmenu").hide();
-                        }
                     }
                 }
-            })    */
+            })
         }
-    }else{
-
-        window.location = "http://localhost:8080/frontend/app/index.html#/myProfile";
     }
+}
+])
+;
 
-}]);
 
 carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.avatarsrc = '../app/img/avatar.JPG';
@@ -105,20 +168,19 @@ carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', function (
     }).success(function (response) {
             obj = response;
             console.log(obj)
-            if(obj.hasOwnProperty("error")){
-                if(obj["error"] == "AuthorizationNeeded") {
+            if (obj.hasOwnProperty("error")) {
+                if (obj["error"] == "AuthorizationNeeded") {
                     window.location = "http://localhost:8080/frontend/app/index.html#/login";
                     $("#error").text("Authorization needed, please log in");
                     $("#error").show();
                 }
-            }else {
+            } else {
                 username = obj["name"];
                 $scope.personname = username
                 var date = obj["dateOfBirth"];
                 $scope.dateBirth = date["day"] + "/" + date["month"] + "/" + date["year"];
                 $scope.cars = obj["cars"];
                 console.log(JSON.stringify(obj["dateOfBirth"]))
-
 
 
             }
@@ -128,37 +190,40 @@ carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', function (
 
     /*$.ajax({
 
-        url: rootUrl + "authorized/myprofile/",
-        method: "GET",
-        success: function (response, $scope) {
-            console.log(response);
-            var obj = JSON.parse(response);
-            console.log(obj)  ;
-            if(obj.hasOwnProperty("error")){
-                if(obj["error"] == "AuthorizationNeeded") {
-                    window.location = "http://localhost:8080/frontend/app/index.html#/login";
-                    $("#error").text("Authorization needed, please log in");
-                    $("#error").show();
-                }
-            }else {
-                username = obj["name"];
-                console.log(String("Hij komt hier"));
+     url: rootUrl + "authorized/myprofile/",
+     method: "GET",
+     success: function (response, $scope) {
+     console.log(response);
+     var obj = JSON.parse(response);
+     console.log(obj);
+     if (obj.hasOwnProperty("error")) {
+     if (obj["error"] == "AuthorizationNeeded") {
+     window.location = "http://localhost:8080/frontend/app/index.html#/login";
+     $("#error").text("Authorization needed, please log in");
+     $("#error").show();
+     }
+     <<<<<<< Updated upstream
+     }else {
+     username = obj["name"];
+     console.log(String("Hij komt hier"));
 
 
-                $scope.$apply(function(){
-                    $scope.personname = "Test";
-                });
+     $scope.$apply(function(){
+     $scope.personname = "Test";
+     });
 
+     =======
+     } else {
+     $scope.name = obj["name"];
+     >>>>>>> Stashed changes
 
-            }
+     }
+     }
+     });
+     <<<<<<< Updated upstream
 
-
-        }
-    });
-
-    console.log(String(username));
-    $scope.personname = String(username);      */
-
+     console.log(String(username));
+     $scope.personname = String(username);      */
 
 }]);
 
@@ -166,10 +231,10 @@ carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', function (
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
