da7cf1432493143299df551efbf134c2e37c3142
Peter Wuyts <peter.wuyts@student.kdg.be>
2014-02-21 16:17:10 +0100

    Angularized addRoute

--- frontend/src/main/webapp/app/js/controllers.js
+++ frontend/src/main/webapp/app/js/controllers.js
@@ -322,6 +322,29 @@ carpoolingControllers.controller('changeRemoveCarCtrl', ['$scope', '$routeParams
         });
 }]);
 
+carpoolingControllers.controller('addRouteCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    console.log("hey addRoute controller test");
+
+    $scope.passages = {};
+
+    $scope.addTime = function() {
+        console.log("Angular: " + $('#days').find(':input:checked').length);
+
+        $('#days').find(':input:checked').each(function () {
+            var time = [];
+            time.push($('#repDepTime').val(), $('#repArrTime').val());
+
+            passages[$(this).data('day')] = time;
+
+            $(this).prop('checked', false);
+
+            $scope.passages = passages;
+        });
+    }
+
+    initialize();
+}]);
+
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
