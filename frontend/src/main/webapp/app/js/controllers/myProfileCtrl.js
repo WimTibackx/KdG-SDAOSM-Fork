643ab1272a0b321497a3c4df5faa3c2d2fb007e7
warri93 <melissa.warrens@gmail.com>
2014-02-26 09:34:27 +0100

    my profile frontend

--- frontend/src/main/webapp/app/js/controllers/myProfileCtrl.js
+++ frontend/src/main/webapp/app/js/controllers/myProfileCtrl.js
@@ -41,6 +41,7 @@ carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', '$location
 
     $scope.hideRoutes = true;
     $scope.hideTrajects = true;
+    $scope.addText = "Voeg auto toe";
 
     $scope.removeCar = function (carId) {
         console.log(carId);
@@ -55,15 +56,18 @@ carpoolingControllers.controller('myProfileCtrl', ['$scope', '$http', '$location
         $scope.hideCars = true;
         $scope.hideRoutes = false;
         $scope.hideTrajects = true;
+        $scope.addText = "Voeg route toe";
     }
     $scope.tabCarsClick = function () {
         $scope.hideCars = false;
         $scope.hideRoutes = true;
         $scope.hideTrajects = true;
+        $scope.addText = "Voeg auto toe";
     }
     $scope.tabTrajectsClick = function () {
         $scope.hideCars = true;
         $scope.hideRoutes = true;
         $scope.hideTrajects = false;
+        $scope.addText = "Voeg traject toe";
     }
 }]);
