d7e8cad9984e17e5ffc95724ad7f238463a01fe6
warri93 <melissa.warrens@gmail.com>
2014-03-11 16:15:50 +0100

    reset password frontend + send to backend

--- frontend/src/main/webapp/app/js/app.js
+++ frontend/src/main/webapp/app/js/app.js
@@ -51,6 +51,9 @@ carpoolingApp.config(['$routeProvider', '$controllerProvider',
             when('/inbox', {
                 templateUrl: 'partials/inbox.html'
             }).
+            when('/resetPassword', {
+                templateUrl: 'partials/resetPassword.html'
+            }).
             otherwise({
                 redirectTo: '/login'
             });
