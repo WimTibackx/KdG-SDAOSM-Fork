733f9ddc9451e8ebdb92c190dc3fa4d1e9ec53a3
Tim Van Roeyen <tim.vanroeyen@student.kdg.be>
2014-03-14 18:56:09 +0100

    Minor functionality tweak to still allow driver messanging, not passenger messaging
    Signed-off-by: Tim Van Roeyen <tim.vanroeyen@student.kdg.be>

--- android/src/be/kdg/groepa/android/activity/UpcomingTrajectsActivity.java
+++ android/src/be/kdg/groepa/android/activity/UpcomingTrajectsActivity.java
@@ -99,6 +99,7 @@ public class UpcomingTrajectsActivity extends ListActivity implements Observer, 
         if (card.getTraject().getChauffeurId() != privPref.getInt("UserId", -1)) {
             btnConfirmRide.setClickable(false);
             btnConfirmRide.getBackground().setAlpha(128);
+            btnSendMessage.setVisibility(View.VISIBLE);
             btnSendMessage.setText("Message driver");
             btnSendMessage.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
@@ -113,6 +114,8 @@ public class UpcomingTrajectsActivity extends ListActivity implements Observer, 
         } else {
             btnConfirmRide.setClickable(true);
             btnConfirmRide.getBackground().setAlpha(255);
+            // Disabling messaging passengers because the functionality is not completely implemented.
+            btnSendMessage.setVisibility(View.INVISIBLE);
             btnSendMessage.setText("Message passengers");
             btnSendMessage.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
